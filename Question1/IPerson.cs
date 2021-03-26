fa9a8fa1b0c64dce05a3a29546ad505ace744d90
Jephren <54954891+JephrenNaicker@users.noreply.github.com>
2021-03-26 16:05:03 +0200

    Add files via upload

---
+++ Question1/IPerson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1
+{
+    interface IPerson
+    {
+
+
+        int CheckSum(string val);
+
+        bool IsSA(string val);
+
+        DateTime getDOB(string val);
+
+        string GetGender(string val);
+    }
+}
---
+++ Question2/IStringPosition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    interface IStringPosition
+    {
+        char[] ConvertToArrChar(string val);
+
+       void DisplayList(List<StringPosition> xList);
+    } 
+}
