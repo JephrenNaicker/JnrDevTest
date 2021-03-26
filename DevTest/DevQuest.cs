c7c6240c65a6e7bb06edb8c780a59c5f2c17dff7
Jephren <54954891+JephrenNaicker@users.noreply.github.com>
2021-03-26 16:06:14 +0200

    Add files via upload

---
+++ DevTest/DevQuest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using System.Reflection;
+
+
+namespace DevTest
+{
+    [TestFixture]
+    public class DevQuest
+    {
+
+      
+
+
+        [Test]
+            public void SampleTest()
+        {
+
+            int input = 1;
+
+            string output ="1";
+
+            Assert.AreEqual("1", output);
+        }
+          
+
+
+    }
+}
