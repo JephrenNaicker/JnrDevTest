06131519e0f9d0b33551372f17c4f71ed5b71bb1
Jephren <54954891+JephrenNaicker@users.noreply.github.com>
2021-03-29 11:06:27 +0200

    Add files via upload

---
+++ Question1Tests/PersonBuilderTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Question1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1.Tests
+{
+    [TestClass()]
+    public class PersonBuilderTests
+    {
+
+        PersonBuilder pBuilder = new PersonBuilder();
+
+        public string input = "";
+
+        [TestMethod()]
+        public void CheckSumTest()
+        {
+            input = "85";
+
+            int output = pBuilder.CheckSum(input);
+
+
+
+            Assert.AreEqual(1, output);
+        }
+
+        [TestMethod()]
+        public void getDOBTest()
+        {
+            input = "950705";
+
+            DateTime output = pBuilder.getDOB(input);
+
+            Assert.AreEqual("1995/07/05 00:00:00", output.ToString());
+
+        }
+
+        [TestMethod()]
+        public void GetGenderTest()
+        {
+            input = "7508305802089";
+
+            string output = pBuilder.GetGender(input);
+
+            Assert.AreEqual("Male", output.ToString());
+
+        }
+
+        [TestMethod()]
+        public void IsSATest()
+        {
+            input = "7508305802089";
+
+            bool output = pBuilder.IsSA(input);
+
+            Assert.IsTrue(output);
+
+        }
+    }
+}
