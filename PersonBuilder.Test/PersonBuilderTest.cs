e99c069a5e33b8f8bfac33ddd7a60e7171d0d2ea
Jephren <54954891+JephrenNaicker@users.noreply.github.com>
2021-03-29 11:07:28 +0200

    Add files via upload

---
+++ PersonBuilder.Test/PersonBuilderTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Question1;
+
+namespace PersonBuilder.Test
+{
+    [TestClass]
+    public class PersonBuilderTest
+    {
+
+
+        [ClassInitialize]
+        public void SetUp()
+        {
+            _loggingService = new PersonBuilder();
+            _userManager = new UserManager();
+        }
+
+
+        [TestMethod]
+        public void Check_If_ID_isVaild()
+        {
+            string input = "25";
+            var expected = 1;
+            var x = '1';
+
+
+            PersonBuilder s = new PersonBuilder();
+
+        }
+    }
+}
