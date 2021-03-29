d182172f7bb1a98ffb121667059d78e6af7a0290
Jephren <54954891+JephrenNaicker@users.noreply.github.com>
2021-03-29 11:05:32 +0200

    Add files via upload

---
+++ Question1/PersonBuilder.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question1
+{
+    public class PersonBuilder : IPerson
+    {
+
+        bool isSA = false;
+
+       
+        public int CheckSum(string val)
+        {
+            char[] ValArr = val.ToCharArray();
+            int[] CSumArr = new int[val.Length];
+
+
+            //Convert char ->int Array
+            for(int i = 0;i<val.Length;i++)
+            {
+
+                CSumArr[i] = Convert.ToInt32(ValArr[i]);
+            }
+
+
+            for(int i = 0; i < val.Length; i++)
+            {
+
+                int temp = CSumArr[i];
+                temp = temp * 2;
+                    if (temp> 9)
+                {
+                    temp = temp % 10 + 1;
+                }
+
+                CSumArr[i] = temp;
+
+            }
+
+
+            int tot = 0;
+            for(int i = 0; i < val.Length; i++)
+            {
+                tot += CSumArr[i];
+            }
+
+            if (tot % 10 == 0)
+            {
+                return 1;
+            }
+            
+                return 0;
+            
+        }
+
+        public DateTime getDOB(string val)
+        {
+            string DOB = val.Substring(0, 6);
+
+            var formattedDate = string.Format("{0:yyMMdd}", DOB);
+           return DateTime.ParseExact(formattedDate, "yyMMdd", CultureInfo.InvariantCulture);
+        }
+
+        public string GetGender(string val)
+        {
+            string gender = val.Substring(6, 4);
+
+            if (float.Parse(gender) <= 4999)
+            {
+                gender = "Female";
+            }
+            else
+            {
+                gender = "Male";
+            }
+
+            return gender;
+        }
+
+        public bool IsSA(string val)
+        {
+            if (int.Parse(val.Substring(10, 1)) == 0)
+            {
+                isSA = true;
+            }
+
+            return  isSA;
+        }
+
+
+
+
+    }
+}
---
+++ Question1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Question1
+{
+    class Program
+    {
+        // Question 1: 
+        // The purpose of this question is to write a method to extract information 
+        // about a person based on their ID number.
+        /*
+            Write an algorithm to extract:
+                1. The persons date of birth
+                2. Gender
+                3. Citizenship
+                4. If the ID Number is valid
+             A South African ID Number is broken down as follows:
+                - The first 6 digits are the persons date of birth in the format YYMMDD
+                - The next 4 digits indicate gender:
+                    >> 0000 - 4999 = FEMALE
+                    >> 5000 - 9999 = MALE
+                - The 11th digit indicates citizenship of the person - 
+                    >> 0 indicates a South African citizen
+                    >> 1  indicates a foreign citizen
+                - The last to digits are a checksum based on Luhn algorithm, this is used to validate the ID number
+        */
+
+        // NOTE: Please include comments in your code
+
+        static void Main(string[] args)
+        {
+            var idNumberList = new List<string>
+            {
+                "7508305802089",
+                "7512305802089",
+                "9507050010085"
+            };
+        
+
+            Program pb = new Program();
+
+            foreach (var id in idNumberList)
+            {
+                var person = pb.ValidateAndExtractInformation(id);
+                Console.WriteLine($"Details for person with ID Number: {id}");
+                Console.WriteLine($"Person Date Of Birth: {person.DateOfBirth.ToString()}");
+                Console.WriteLine($"Person Gender: {person.Gender}");
+                Console.WriteLine($"Is person a South African Citizen: {person.isSouthAfricanCitizen.ToString()}");
+                Console.WriteLine($"Is ID Number Valid: {person.isValidIDNumber}");
+                Console.WriteLine($"**************************************************");
+            }
+
+            Console.ReadLine();
+        }
+
+        public Person ValidateAndExtractInformation(string idNumber)
+        { 
+            PersonBuilder personBuilder = new PersonBuilder();
+            bool isVal = false;
+            
+            if (personBuilder.CheckSum(idNumber.Substring(11, 2)) == 1)
+            {
+                isVal = true;
+            }
+
+
+            Person p = new Person();
+            p.DateOfBirth = personBuilder.getDOB(idNumber);
+            p.Gender = personBuilder.GetGender(idNumber);
+            p.isSouthAfricanCitizen = personBuilder.IsSA(idNumber);
+            p.isValidIDNumber = isVal;
+
+            return p;
+
+
+        }
+
+    }
+}
