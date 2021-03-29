901c285a6b2c82440d2d153330fbb4a74f34521c
Jephren <54954891+JephrenNaicker@users.noreply.github.com>
2021-03-29 11:01:42 +0200

    added missing Files

---
+++ Question2/StringPostionBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    class StringPostionBuilder : IStringPosition
+    {
+
+        public char[] ConvertToArrChar(string val)
+        {
+            char[] ValArr = val.ToCharArray();
+            return ValArr;
+        }
+
+        public void DisplayList(List<StringPosition> xList)
+        {
+            if (xList.Count == 0)
+            {
+                Console.WriteLine("No Duplicated Letter");
+
+            }
+            else
+            {
+
+                foreach (var x in xList)
+                    Console.WriteLine("DuplicatedLetter = {0},DuplicatedPosition={1}", x.DuplicatedLetter, x.DuplicatedPosition);
+            }
+        }
+
+       
+    }
+}
